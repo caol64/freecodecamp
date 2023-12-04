249e60aaa2f2ee4e76ba18105b3be8630e0e714d
Lei <caol64@gmail.com>
2023-12-04 16:18:37 +0800

    add

---
+++ Back End Development and APIs/04.boilerplate-project-timestamp/index.js
@@ -0,0 +1,44 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+require('dotenv').config();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/:date", function (req, res) {
+  const { date } = req.params; // equivalent to const date = req.params.date;
+  const numericValue = Number(date);
+  let dateObj;
+  if (isNaN(numericValue)) {
+    dateObj = new Date(date);
+  } else {
+    dateObj = new Date(numericValue);
+  }
+  res.json({
+    unix: dateObj.getTime(),
+    utc: dateObj.toUTCString(),
+  });
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
